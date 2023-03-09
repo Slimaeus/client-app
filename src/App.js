bf7a4adb50242f341cad745eebec7a8f466c1801
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-03-09 15:15:29 +0700

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [missions, setMissions] = useState([])
+  useEffect(() => {
+    axios.get("https://plantogetherdotnetapi.azurewebsites.net/api/Missions")
+      .then(response => setMissions(response.data))
+      .catch(error => console.log(error))
+  }, [missions])
+
+  function getPriority(priority)
+  {
+    switch (priority)
+    {
+      case 0:
+        return 'Low'
+      case 1:
+        return 'Medium'
+      case 2:
+        return 'High'
+      default:
+        return ''
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {missions.map((m) =>
+        <div key={m.Id} class="p-card">
+          <h3>{m.Title}</h3>
+          <p class="p-card__content">{m.Description}</p>
+          <p class="p-card__content">{getPriority(m.Priority)}</p>
+        </div>
+      )}
     </div>
   );
 }
